9bcb95e1b7dd61c40e846c4ba5d7e32771d98c85
John Suico <johncdsuico@gmail.com>
2020-09-10 08:41:27 -0700

    add db connection and env vars

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 
+require('dotenv').config();
+
 app.use(express.json());
 app.use(cors());
 
@@ -10,8 +12,16 @@ app.get('/', (req, res) => {
   res.send('You made it to the api');
 });
 
-const PORT = 5000 | process.env.PORT;
+// MONGO_URI is a environment variable. Use the "dotenv" package to set this up on your local system.
+// If you need help with this, contact John
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.on('connected', () => {
+  console.log("Successfully connected to MongoDB");
+});
+mongoose.connection.on('error', console.error.bind(console, 'Connection error: '));
 
-app.listen(PORT, (req, res) => {
-  console.log(`Backend server started on port: ${PORT}`);
+// process.env.PORT is a environment variable. Needs to be setup with "dotenv"
+// Contact John for help
+app.listen(process.env.PORT, (req, res) => {
+  console.log(`Backend server started on port: ${process.env.PORT}`);
 })
