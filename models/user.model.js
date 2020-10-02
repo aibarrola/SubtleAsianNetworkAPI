e6593affe3c0bb6916874212bc0f8f0a0bc80f23
John Suico <johncdsuico@gmail.com>
2020-10-01 19:16:10 -0700

    remove user model tweak

--- models/user.model.js
+++ models/user.model.js
@@ -3,13 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema ({
   firstName: {
     type: String,
-    required: true,
-    default: ''
+    required: true
   },
   lastName:{
     type: String,
-    required: true,
-    default: ''
+    required: true
   },
   hashedPassword: {
     type: String,
