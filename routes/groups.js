21f8022c3d6b640c6ed590165534d048124fd8c2
James Nguyen <nguyen.james615@gmail.com>
2020-10-11 19:29:28 -0700

    added authentication to group postings, but left it public for testing purposes

--- routes/groups.js
+++ routes/groups.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const Group = require('../models/group.model');
 const Post = require('../models/post.model');
 
+// Middle ware
+const auth = require('./middleware/auth');
+
+
 // @Route   GET /groups/
 // @desc    Returns all groups
 // @access  Public
@@ -22,17 +26,24 @@ router.route('/add').post((req,res)=>{
     .catch(err => res.status(400).json('Error: ' + err))
 })
 
-router.route('/post/:id/add').post((req,res)=>{
+// @Route   POST /groups/post/:id/add
+// @desc    Adds a post to a specific group
+// @access  Private BUT PUBLIC FOR TESTING NOW
+router.route('/post/:id/add').post(/**auth,**/(req,res)=>{
     const id = req.params.id;
     const{author, date, type, content} = req.body;
 
     Group.findById(req.params.id)
     .then(group =>{
+       /**  if(group.users.include(req.user.user_id)){**/
         const posts = group.posts;
         posts.push({author,date,type, content});
         group.save()
         .then(()=>res.json("Post added"))
         .catch(err => res.status(400).json('Error: ' + err))
+       /**  }else{
+            res.status(400).json({ msg: 'User not authenticated to post'})
+        }**/
     })
 
 })
