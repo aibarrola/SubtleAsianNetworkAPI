bf7105329a26fd8d12f437cde241def0c469f1f0
John Suico <johncdsuico@gmail.com>
2021-01-28 16:44:36 -0800

    change res send in cohert

--- routes/cohert.js
+++ routes/cohert.js
@@ -2,19 +2,17 @@ const Cohert = require('../models/cohert.model');
 const router = require('Express').Router();
 
 router.route('/').post((req, res) =>{
-    // const {school, organization, leadID} = req.body;
-    // const newCohert = new Cohert({school, organization, leadID});
-    // newCohert.save()
-    // .then(res.json("Successfully Created"))
-    // .catch(err=> res.status(400).json("Error: "+ err))
-
-    res.send('Cohert route');
-
+    const {school, organization, leadID} = req.body;
+    const newCohert = new Cohert({school, organization, leadID});
+    newCohert.save()
+    .then(res.json("Successfully Created"))
+    .catch(err=> res.status(400).json("Error: "+ err))
 });
 
 router.route('/').get((req,res)=>{
-Cohert.find({})
-.then(coherts => res.send(coherts));
+  // Cohert.find({})
+  // .then(coherts => res.send(coherts));
+  res.send('Cohery route');
 })
 
 module.exports = router;
