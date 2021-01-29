0d9d392c602d7b2d998258b2152a0bde3eb43831
John Suico <johncdsuico@gmail.com>
2021-01-28 16:35:03 -0800

    change cohert route for test

--- routes/cohert.js
+++ routes/cohert.js
@@ -2,11 +2,14 @@ const Cohert = require('../models/cohert.model');
 const router = require('Express').Router();
 
 router.route('/').post((req, res) =>{
-    const {school, organization, leadID} = req.body;
-    const newCohert = new Cohert({school, organization, leadID});
-    newCohert.save()
-    .then(res.json("Successfully Created"))
-    .catch(err=> res.status(400).json("Error: "+ err))
+    // const {school, organization, leadID} = req.body;
+    // const newCohert = new Cohert({school, organization, leadID});
+    // newCohert.save()
+    // .then(res.json("Successfully Created"))
+    // .catch(err=> res.status(400).json("Error: "+ err))
+
+    res.send('Cohert route');
+
 });
 
 router.route('/').get((req,res)=>{
